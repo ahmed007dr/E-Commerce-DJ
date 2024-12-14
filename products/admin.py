588cbfaed6c2b073230b48ee9e26cfef5ead2c04
ahmed <ahmed007eg@gmail.com>
2024-12-14 16:12:46 +0200

    merge to table tool inlines admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product , Brand
+from .models import Product , Brand , ProdcutImages ,Review
 
-admin.register.site(Product)
-admin.register.site(Brand)
+
+class ProductImagesInLine(admin.TabularInline):
+    model = ProdcutImages  # استخدم الاسم الصحيح للموديل
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesInLine]  # إضافة الـ inlines لعرض الصور داخل المنتج
+
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Brand)
+admin.site.register(Review)
