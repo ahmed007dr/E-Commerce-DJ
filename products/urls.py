daec92cae49c53f49390858bb57f5eaea5c4341b
ahmed <ahmed007eg@gmail.com>
2024-12-17 19:17:28 +0200

    create url with CBV

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+from .views import ProductList , ProductDetail
+from django.urls import path
+
+
+urlpatterns = [
+    path('',ProductList.as_view()),
+    path('<slug:slug>',ProductDetail.as_view()),
+
+] 
