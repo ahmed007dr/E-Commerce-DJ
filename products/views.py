2389ed28288ca59f7a4ee0f8b3b199fb64ca70c5
ahmed <ahmed007eg@gmail.com>
2024-12-20 20:17:36 +0200

    show product with review from view Context

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 # Create your views here.
@@ -7,5 +8,9 @@ class ProductList(ListView):
     model = Product
 
 
-class ProductDetail(DetailView):
+class ProductDetail(DetailView): # context() - Quryset : product.object.all() : 1:optaion 2:method : orverride
     model = Product
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs) # dict : object product
+        context ['reviews'] = Review.objects.filter(product=self.get_object())
+        return context
