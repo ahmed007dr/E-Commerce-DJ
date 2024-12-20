47c26ca9c46927611eb9de63d01f109246e5376c
ahmed <ahmed007eg@gmail.com>
2024-12-20 22:33:48 +0200

    add brand details

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('brand/',BrandList.as_view()),
-    path('brand/<slug:slug>',BrandDetail.as_view()),
+    path('brand/<slug:slug>/',BrandDetail.as_view(), name='brand_detail'),
 
     path('',ProductList.as_view()),
     path('<slug:slug>',ProductDetail.as_view()),
--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 # Create your views here.
 from .models import Product , Brand , Review ,ProductImages
+from django.shortcuts import get_object_or_404
 
 class ProductList(ListView):
     model = Product
@@ -23,5 +25,27 @@ class BrandList(ListView):
     model = Brand
 
 
-class BrandDetail(DetailView):
-    pass
+class BrandDetail(ListView):
+    model = Product
+    template_name = 'products/brand_detail.html'
+
+    def get_queryset(self):
+        # Ensure we're fetching the correct brand based on the slug
+        brand = get_object_or_404(Brand, slug=self.kwargs['slug'])
+        # Return products that belong to this brand
+        return Product.objects.filter(brand=brand)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add the brand to the context so we can display its name and details
+        context['brand'] = get_object_or_404(Brand, slug=self.kwargs['slug'])
+        return context
+
+
+# class BrandDetail(DetailView):
+#     model = Brand
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)  
+#         context['products'] = Product.objects.filter(brand = self.get_object())
+
+#         return context
