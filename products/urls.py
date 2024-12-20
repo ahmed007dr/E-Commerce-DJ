906fda1b458c167ffa0bd1e6657f5796adc9a62e
ahmed <ahmed007eg@gmail.com>
2024-12-20 23:36:40 +0200

    fix url

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,9 @@
-from .views import ProductList , ProductDetail ,BrandList,BrandDetail
 from django.urls import path
-
+from .views import ProductList, ProductDetail, BrandList, BrandDetail
 
 urlpatterns = [
-    path('brand/',BrandList.as_view()),
-    path('brand/<slug:slug>/',BrandDetail.as_view(), name='brand_detail'),
-
-    path('',ProductList.as_view()),
-    path('<slug:slug>',ProductDetail.as_view()),
-
-] 
+    path('brands/', BrandList.as_view()),  # URL pattern for listing all brands
+    path('brands/<slug:slug>/', BrandDetail.as_view()),  # URL pattern for a specific brand
+    path('', ProductList.as_view()),  # URL pattern for the product list
+    path('<slug:slug>', ProductDetail.as_view()),  # URL pattern for a specific product
+]
