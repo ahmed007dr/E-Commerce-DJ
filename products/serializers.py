2d0a8377cfe6ff413e984814b44b66b9fa7ab4f0
ahmed <ahmed007eg@gmail.com>
2024-12-24 02:51:35 +0200

    add avrage rate column API

--- products/serializers.py
+++ products/serializers.py
@@ -6,28 +6,53 @@ class ProductSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
 
     review_count = serializers.SerializerMethodField() # add new column in API view url <<<<<<<<
+    average_rate = serializers.SerializerMethodField()  # Corrected method name
 
     class Meta:
         model = Product
         fields = "__all__"
 
-    def get_review_count(self, obj): #get_{field_name}
+    def get_review_count(self, object): #get_{field_name}
         reviews = object.review_product.all().count() # review_product دا اسم العلاقه اللي في جدول التقيمات
         return reviews
+
+    def get_average_rate(self,object):
+        total = 0 # sum rate : object
+        reviews = object.review_product.all()
+        if len(reviews) > 0 :
+            for item in reviews:
+                total += item.rate # rate column in reviews
+            avg = total / len (reviews)
+        else:
+            avg = 0
+        # print(f"Average rate for product {object.id}: {avg}")  
+        return avg
 
 
 class ProductDetailsSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
     review_count = serializers.SerializerMethodField() # add new column in API view url <<<<<<<<
+    average_rate = serializers.SerializerMethodField()  # Corrected method name
 
     class Meta:
         model = Product
         fields = "__all__"
 
-    def get_review_count(self, obj): #get_{field_name}
+    def get_review_count(self, object): #get_{field_name}
         reviews = object.review_product.all().count() # review_product دا اسم العلاقه اللي في جدول التقيمات
         return reviews
 
+    def get_average_rate(self,object):
+        total = 0 # sum rate : object
+        reviews = object.review_product.all()
+        if len(reviews) > 0 :
+            for item in reviews:
+                total += item.rate # rate column in reviews
+            avg = total / len (reviews)
+        else:
+            avg = 0
+        # print(f"Average rate for product {object.id}: {avg}")  
+        return avg
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
