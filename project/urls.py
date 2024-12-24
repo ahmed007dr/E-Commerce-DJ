7085204e1e8fcbb69c1f3de02f133e3a94533aa2
ahmed <ahmed007eg@gmail.com>
2024-12-24 03:22:32 +0200

    add swagger settings

--- project/urls.py
+++ project/urls.py
@@ -19,10 +19,30 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+# تعريف schema_view باستخدام drf-yasg
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Swagger API",
+      default_version='v1',
+      description="API documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@myapi.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/',include('products.urls')),
+    path('products/', include('products.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
