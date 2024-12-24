b1cf07abdc5829b13e261df2a7043027239b7778
ahmed <ahmed007eg@gmail.com>
2024-12-24 02:10:22 +0200

    add new column in API view review count

--- products/api.py
+++ products/api.py
@@ -1,7 +1,6 @@
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-
 from .models import Product,Brand,Review,ProductImages
 from . import serializers
 
@@ -10,7 +9,6 @@ from . import serializers
 class ProductListAPI(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductDetailsSerializer
-
     filter_backends = [DjangoFilterBackend,filters.SearchFilter]
     filterset_fields = ['flag', 'name','price']
     search_fields = ['name', 'price']
--- products/serializers.py
+++ products/serializers.py
@@ -1,12 +1,20 @@
-from rest_framework import serializers
+from rest_framework import serializers 
 
-from .models import Product, Brand
+from .models import Product, Brand, Review
 
 class ProductSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
+
+    review_count = serializers.SerializerMethodField() # add new column in API view url <<<<<<<<
+
     class Meta:
         model = Product
         fields = "__all__"
+
+    def get_review_count(self, obj): #get_{field_name}
+        reviews = object.review_product.all().count() # review_product دا اسم العلاقه اللي في جدول التقيمات
+        return reviews
+
 
 class ProductDetailsSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
