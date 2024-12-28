c4493200114c56ae8709acd6be119dd0593bfa83
ahmed <ahmed007eg@gmail.com>
2024-12-28 16:07:53 +0200

    order_by with filter user

--- orders/views.py
+++ orders/views.py
@@ -6,12 +6,13 @@ from .models import Order , OrderDetail,Cart,CartDetail,Coupon
 from django.core.paginator import Paginator
 
 def order_list(request):
-    orders = Order.objects.filter(user=request.user)
-
+    orders = Order.objects.filter(user=request.user).order_by('-order_time')  
+    
     paginator = Paginator(orders, 10)  
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
+    print (orders)
     return render(request, 'orders/order_list.html', {'page_obj': page_obj , 'orders':orders})
 
 def checkout(request):
