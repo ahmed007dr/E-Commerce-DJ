8bc6f7039acf060e914be150d2f73da5c2516e23
ahmed <ahmed007eg@gmail.com>
2025-01-01 01:30:51 +0200

    create basic view and model.py for user

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+from utils.generate import generate_code
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+
+class Profile(models.Model): # VIDEO 41
+    user = models.OneToOneField(User,related_name='profile',on_delete=models.CASCADE)
+    image =models.ImageField(upload_to='profile')
+    code = models.CharField(max_length=10,default=generate_code)
+
+    def __str__(self):
+        return str(self.user)
+
+@receiver(post_save,sender=User) # when create new user >> automatic create profile
+def create_profile(sender,instance,created,*args, **kwargs): # create profile 
+    if created:
+        Profile.objects.create(
+            user = instance,
+        )
+
+PHONE_TYPE=(
+    ('primary','primary'),
+    ('secondary','secondary'),
+    ('office','office'),
+)
+class ContactNumbers(models.Model): # VIDEO 41
+    user = models.ForeignKey(User,related_name='user_contact',on_delete=models.CASCADE)
+    type = models.CharField(max_length=10,choices=PHONE_TYPE)
+    number = models.CharField(max_length=25)
+
+
+
 
 
 ADDRESS_TYPE=(
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def sigup(request): # VIDEO 41
+    pass
+
+
+
+
+
+def user_activate(request): # VIDEO 41
+    pass
